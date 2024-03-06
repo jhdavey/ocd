818f1f63d41f22158cacc04c9d03ace701280752
jhdavey <jhdavey5182@gmail.com>
2024-03-06 16:12:23 -0500

    Updated favicon

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
+import Faqs from './components/pages/Faqs';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 
@@ -11,6 +12,7 @@ function App() {
       <NavBar />
         <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/faqs' element={<Faqs />} />
         {/* Catch all path if user navigates to a path that does not exist */}
           <Route path='*' element={<h1 className="display-2">Wrong page!</h1>} />
         </Routes>
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,14 +4,13 @@ export default function NavBar() {
     alert('We are not currently accepting new detailers.');
   };
 
-  const reload = () => {
-    window.location.reload();
-  };
-
   return (
-    <div className="p-5 md:flex md:items-center md:justify-between">
-      <h1 className="text-red-600 font-bold text-2xl md:text-[36px]" onClick={reload}>OrlandoCarDetailing.com</h1>
-      <button className="bg-white border border-black rounded-2xl px-4 m-1 font-bold" onClick={handleClick}>Join our Pro Network</button>
+    <div className="p-3 md:flex md:items-center md:justify-between">
+      <a href="/" className="text-red-600 font-bold text-2xl md:text-[36px]">Orlando Car Detailing</a>
+      <div>
+        <button className="bg-white border border-black rounded-2xl px-4 m-1 font-bold" onClick={handleClick}>Join our Pro Network</button>
+        <a className="bg-white border border-black rounded-2xl px-4 m-1 font-bold" href='/faqs'>FAQs</a>
+      </div>
     </div>
   )
 }
